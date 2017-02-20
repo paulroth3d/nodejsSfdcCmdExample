7b5162aef3a2eb49996216803ec151c24e0b5f16
Paul Roth <paulroth3d@gmail.com>
2017-02-20 15:28:08 +0000

    UPDATE; add in commander to the file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
 #!/usr/bin/env node
 
-///<reference path='../typings/globals/require/index.d.ts' />
+//-- this is an old line that I might be able to remove - ///<reference path='../typings/globals/require/index.d.ts' />
 
 'use strict';
 
 let pkg:any = require( '../package.json' );
 
+//-- @TODO: remove
 console.log( 'package.name:' + pkg.name );
-//typings/globals/require/index.d.ts
+debugger;
+console.log( 'something else' );
+
+/** command line argument handling **/
+var program:any = require('commander');
+
+program
+        .version('0.0.1')
+        .option( '-l, --login', 'start the login process' )
+        .option( '-s, --sandbox', 'use this parameter if you need to connect to a sandbox' )
+        .option( '-h, --host [domain]', 'use this to connect to a custom domain' )
+        .option( '-p, --put [somevalue]', 'test put and getting values' )
+        .option( '-g, --get', 'gets the value from the config store' )
+        .option( '-o, --logout', 'logs the current user out' )
+        .parse( process.argv ); //-- always end with a parse
+
+program.on( '--help', function(){
+	console.log( '' + pkg.description );
+	console.log( '[' + pkg.version + ']' );
+	
+	program.help();
+});
+
+console.log( 'login:' + program.login );

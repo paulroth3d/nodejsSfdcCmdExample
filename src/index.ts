dbe0eb5359c13c58aa515ff24545f3da0e912ce6
Paul Roth <paulroth3d@gmail.com>
2017-03-26 23:30:49 +0000

    UPDATE: include tslint

--- src/index.ts
+++ src/index.ts
@@ -45,14 +45,14 @@ let isConnected:boolean;
 //-- initialize the app.
 
 program
-        .version('0.0.1')
-        .option( '-l, --login', 'start the login process' )
-        .option( '-s, --sandbox', 'use this parameter if you need to connect to a sandbox' )
-        .option( '-h, --host [domain]', 'use this to connect to a custom domain' )
-        .option( '-p, --put [somevalue]', 'test put and getting values' )
-        .option( '-g, --get', 'gets the value from the config store' )
-        .option( '-o, --logout', 'logs the current user out' )
-        .parse( process.argv ); //-- always end with a parse
+	.version('0.0.1')
+	.option( '-l, --login', 'start the login process' )
+	.option( '-s, --sandbox', 'use this parameter if you need to connect to a sandbox' )
+	.option( '-h, --host [domain]', 'use this to connect to a custom domain' )
+	.option( '-p, --put [somevalue]', 'test put and getting values' )
+	.option( '-g, --get', 'gets the value from the config store' )
+	.option( '-o, --logout', 'logs the current user out' )
+	.parse( process.argv ); //-- always end with a parse
 
 program.on( '--help', function(){
 	console.log( '' + pkg.description );

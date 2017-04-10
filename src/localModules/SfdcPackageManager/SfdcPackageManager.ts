79416fc9ef194f0738ab57c49c1095ae5511b17f
Paul Roth <proth@salesforce.com>
2017-04-09 20:57:31 -0500

    UPDATE: placeholder for the SfdcPackageManager

---
+++ src/localModules/SfdcPackageManager/SfdcPackageManager.ts
@@ -0,0 +1,34 @@
+//-- utilize promises
+import * as Q from 'q';
+
+/**
+ * Utility class that is able to convert between salesforce checkout folders
+ * and package files.
+ * @author Paul Roth <proth@salesforce.com>
+ **/
+export class SfdcPackageManager {
+	
+	//-- generates a package of all files in a specific checkout
+	//-- generates a package to deploy everything from a range of commits
+	//-- adds files to a packageList
+		//-- or just pick a local file
+		//-- from a local file by pattern
+		//-- manually
+		//-- from metadata
+	//-- removes a file from a packageList
+	
+	//-- list local packagelist files
+		//-- maybe not...?
+		
+	//-- add two packageLists together
+	//-- remove one packageList from another
+	
+	//-- work with ignore files
+	
+	//-- convert a package file to a packageList
+	
+	//-- deploy from list
+	
+	//-- refresh from a packageList
+	
+}
